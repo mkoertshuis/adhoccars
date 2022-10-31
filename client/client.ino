dd32e9d2cf7eda26eaebd7f3fec89e657fb5afe4
mkoertshuis <mickkoertshuis@gmail.com>
2022-10-31 18:34:44 +0100

    Added clientside control

--- client/client.ino
+++ client/client.ino
@@ -7,6 +7,16 @@ boolean connected = false;
 WiFiUDP udp;
 char packet[255];
 
+const int rf1 = 19;
+const int rf2 = 18;
+const int lf1 = 5;
+const int lf2 = 17;
+const int rb1 = 14;
+const int rb2 = 15;
+const int lb1 = 16;
+const int lb2 = 4;
+const int control[4][2] = { {rf1, rf2}, {rb1, rb2}, {lf1, lf2}, {lb1, lb2} }; 
+
 // Packet format:
 // Opcode [1 b]   | Arguments [n b]
 // ---------------|----------------
@@ -14,43 +24,6 @@ char packet[255];
 // Control (0x01) | Direction (1 b)
 // Leader (0x02)  | Fuse MAC (8 b)
 // Kill (0x03)    | (0 b)
-
-void setup(){
-  // Initilize hardware serial:
-  Serial.begin(115200);
-  
-  //Connect to the WiFi network
-  connectToWiFi(networkName);
-}
-
-void loop(){
-  //only send data when connected
-  if(connected){
-    //Send a packet
-    udp.beginPacket(WiFi.broadcastIP(), udpPort);
-    udp.printf("Seconds since boot: %lu", millis()/1000);
-    udp.write();
-    udp.read();
-    udp.endPacket();
-
-    ESP.getEfuseMac()
-
-    int packetSize = udp.parsePacket();
-    udp.remoteIP()
-    if (packetSize) {
-      Serial.print("Received packet! Size: ");
-      Serial.println(packetSize); 
-      int len = udp.read(packet, 255);
-      if (len > 0)
-      {
-        packet[len] = '\0';
-      }
-      Serial.println(packet);
-    }
-  }
-  //Wait for 1 second
-  delay(1000);
-}
 
 void connectToWiFi(const char * ssid){
   Serial.println("Connecting to WiFi network: " + String(ssid));
@@ -85,17 +58,8 @@ void WiFiEvent(WiFiEvent_t event){
       default: break;
     }
 }
-motors.ino
-const int rf1 = 19;
-const int rf2 = 18;
-const int lf1 = 5;
-const int lf2 = 17;
-const int rb1 = 14;
-const int rb2 = 15;
-const int lb1 = 16;
-const int lb2 = 4;
 
-void setup() {
+void init_pins() {
   pinMode(rf1, OUTPUT);
   pinMode(rf2, OUTPUT);
   pinMode(lf1, OUTPUT);
@@ -106,13 +70,123 @@ void setup() {
   pinMode(lb2, OUTPUT);
 }
 
-void loop() {
-  analogWrite(rf1, 255);
-  analogWrite(rf2, 0);
-  analogWrite(lf1, 127);
-  analogWrite(lf2, 0);
-  analogWrite(rb1, 255);
-  analogWrite(rb2, 0);
-  analogWrite(lb1, 127);
-  analogWrite(lb2, 0);
+void stop() {
+  for ( int i = 0; i < 4; ++i ) {
+    for ( int j = 0; i < 2; ++j ) {
+      analogWrite(control[i][j], 0);
+    }
+  }
+}
+
+void move_forward() {
+  for ( int i = 0; i < 4; ++i ) {
+    analogWrite(control[i][0], 255);
+    analogWrite(control[i][1], 0);
+  }
+}
+
+void move_back() {
+  for ( int i = 0; i < 4; ++i ) {
+    analogWrite(control[i][0], 0);
+    analogWrite(control[i][1], 255);
+  }
+}
+
+void turn_left() {
+  for ( int i = 0; i < 4; ++i ) {
+    if ( i < 2 ) {
+      analogWrite(control[i][0], 255);
+      analogWrite(control[i][1], 0);
+    } else {
+      analogWrite(control[i][0], 0);
+      analogWrite(control[i][1], 255); 
+    }
+  }
+}
+
+void turn_right() {
+  for ( int i = 0; i < 4; ++i ) {
+    if ( i < 2 ) {
+      analogWrite(control[i][0], 0);
+      analogWrite(control[i][1], 255);
+    } else {
+      analogWrite(control[i][0], 255);
+      analogWrite(control[i][1], 0); 
+    }
+  }
+}
+
+void control_handler(uint8_t direction) {
+  switch (direction) {
+    case 1: // forward
+      move_forward();
+      break;
+    case 2: // back
+      move_back();
+      break;
+    case 3: // left
+      turn_left();
+      break;
+    case 4: // right
+      turn_right();
+      break;
+    default: // break
+      stop();
+      break;
+  }
+}
+
+void packet_handler(char * packet) {
+  // Change the char of the packet to a byte for easier handling
+  uint8_t op = (uint8_t) packet[0];
+  switch (op) {
+    case 1: // control
+    {
+      uint8_t direction = (uint8_t) packet[1];
+      control_handler(direction);
+      break;
+    }
+    default:
+      break;
+  }
+}
+
+void setup(){
+  init_pins();
+
+  // Initilize hardware serial:
+  Serial.begin(115200);
+  
+  //Connect to the WiFi network
+  connectToWiFi(networkName);
+}
+
+void loop(){
+  //only send data when connected
+  if(connected){
+    //Send a packet
+    udp.beginPacket(WiFi.broadcastIP(), udpPort);
+    udp.printf("Seconds since boot: %lu", millis()/1000);
+    // udp.write();
+    udp.read();
+    udp.endPacket();
+
+    // ESP.getEfuseMac()
+
+    int packetSize = udp.parsePacket();
+    // udp.remoteIP()
+    if (packetSize) {
+      Serial.print("Received packet! Size: ");
+      Serial.println(packetSize); 
+      int len = udp.read(packet, 255);
+      if (len > 0)
+      {
+        packet[len] = '\0';
+      }
+      Serial.println(packet);
+      packet_handler(packet);
+    }
+  }
+  //Wait for 1 second
+  delay(1000);
 }
