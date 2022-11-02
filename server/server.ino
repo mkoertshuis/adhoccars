1fe46b6b1039019247c7c9910755116eb6220ac1
guusbotman <botman.g@gmail.com>
2022-11-02 10:22:55 +0100

    test server

--- server/server.ino
+++ server/server.ino
@@ -3,6 +3,8 @@
 #include <WiFiAP.h>
 
 const char *ssid = "rssi-cars";
+WiFiUDP udp;
+char incomingPacket[255];  // buffer for incoming packets
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -15,8 +17,23 @@ void setup() {
   IPAddress ip = WiFi.softAPIP();
   Serial.print("AP IP address: ");
   Serial.println(ip);
+  udp.begin(3333);
 
   Serial.println("Server started");
 }
 
-void loop() { }
+void loop() {
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+  {
+    // receive incoming UDP packets
+    // Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0)
+    {
+      incomingPacket[len] = 0;
+    }
+    Serial.printf("UDP packet contents: %s\n", incomingPacket);
+  }
+  delay(1000)
+}
